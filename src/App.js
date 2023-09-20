f41bfee1f610350f44db1c37efdf73143c91f38c
Ivan <ivannissimrch@gmail.com>
2023-09-19 20:03:28 -0400

    added a loader to the product details page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,13 @@ import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/ErrorPage";
 import { useState } from "react";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import { productsDetailLoader } from "./pages/ProductDetailPage";
 
 function App() {
   const [productsOnCart, setProductsOnCart] = useState([]);
-  const [productDetails, setProductDetails] = useState({});
   const productsToShow = 5;
-  // const [amountOfProducts, setAmountOfProducts] = useState(0);
 
-  const showProductDetails = (currentProduct) => {
-    setProductDetails(currentProduct);
-  };
-
-  //add poduct to cart
+  //add product to cart
   const addProductToCart = (product) => {
     //add product to cart with amount of 1
     if (!productsOnCart.some((item) => item.id === product.id)) {
@@ -52,8 +47,6 @@ function App() {
     const updatedCart = [...productsOnCart];
     updatedCart[productIndex].amount = newAmount;
     setProductsOnCart(updatedCart);
-
-    //make this a function
   };
 
   const router = createBrowserRouter([
@@ -64,22 +57,18 @@ function App() {
       children: [
         {
           index: true,
-          element: <HomePage showProductDetails={showProductDetails} />,
+          element: <HomePage />,
           loader: () => productsLoader(productsToShow),
         },
         {
           path: "products",
-          element: <ProductsPage showProductDetails={showProductDetails} />,
+          element: <ProductsPage />,
           loader: productsLoader,
         },
         {
-          path: "/:productId",
-          element: (
-            <ProductDetailPage
-              productDetails={productDetails}
-              addProductToCart={addProductToCart}
-            />
-          ),
+          path: ":productId",
+          element: <ProductDetailPage addProductToCart={addProductToCart} />,
+          loader: productsDetailLoader,
         },
         {
           path: "cart",
--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,20 +1,35 @@
 import Card from "../componnets/Card";
 import classes from "./ProductDetailsPage.module.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
 
-const ProductDetailPage = ({ productDetails, addProductToCart }) => {
+const ProductDetailPage = ({ addProductToCart }) => {
+  const productData = useLoaderData();
+
   const navigate = useNavigate();
   const handleOnClick = () => {
-    addProductToCart(productDetails);
+    addProductToCart(productData);
     navigate("../cart");
   };
 
   return (
     <div className={classes.productDetails}>
-      <Card productData={productDetails} />
-      <p>{productDetails.description}</p>
+      <Card productData={productData} />
+      <p>{productData.description}</p>
       <button onClick={handleOnClick}>Add to cart</button>
     </div>
   );
 };
 export default ProductDetailPage;
+
+async function productsDetailLoader({ params }) {
+  const response = await fetch(
+    `https://fakestoreapi.com/products/${params.productId}`
+  );
+  if (!response.ok) {
+    throw new Response("Error");
+  } else {
+    return response;
+  }
+}
+
+export { productsDetailLoader };
