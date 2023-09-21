d24960c8e0c62a3214c4f005916d0fd260c09e5b
Ivan <ivannissimrch@gmail.com>
2023-09-20 21:13:22 -0400

    added buttons to increase or decrease product amount on CardCart

--- src/componnets/CardCart.js
+++ src/componnets/CardCart.js
@@ -3,6 +3,22 @@ import classes from "./Card.module.css";
 
 const CardCart = ({ productData, onDelete, onEditProductAmount }) => {
   const [amount, setAmount] = useState(productData.amount);
+
+  const handleReduceClick = () => {
+    if (amount <= 1) {
+      onDelete(productData);
+    } else {
+      setAmount((prev) => prev - 1);
+      onEditProductAmount(productData, amount - 1);
+    }
+  };
+
+  const handleIncreaseClick = (event) => {
+    setAmount((prev) => prev + 1);
+
+    onEditProductAmount(productData, amount + 1);
+  };
+
   const handleAmountChange = (event) => {
     if (event.target.value === "0" || event.target.value.includes("-")) {
       onDelete(productData);
@@ -12,9 +28,11 @@ const CardCart = ({ productData, onDelete, onEditProductAmount }) => {
       onEditProductAmount(productData, newAmount);
     }
   };
+
   const handleDelete = () => {
     onDelete(productData);
   };
+
   return (
     <div className={classes["card-container"]}>
       <img className={classes.image} src={productData.image} alt="product" />
@@ -25,14 +43,17 @@ const CardCart = ({ productData, onDelete, onEditProductAmount }) => {
         </button>
         <div>
           <label htmlFor="amountOFProducst"> Amount</label>
-          <input
-            onChange={handleAmountChange}
-            name="amountOfProducts"
-            type="number"
-            step={1}
-            min={0}
-            value={amount}
-          />
+          <div className={classes["amount-container"]}>
+            <button onClick={handleReduceClick}>-</button>
+            <input
+              onChange={handleAmountChange}
+              name="amountOfProducts"
+              type="number"
+              min={0}
+              value={amount}
+            />
+            <button onClick={handleIncreaseClick}>+</button>
+          </div>
         </div>
       </div>
     </div>
